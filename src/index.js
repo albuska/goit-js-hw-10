27db3e6ee9263f44f827e46d8d942d45a74ec6be
albuska <109683293+albuska@users.noreply.github.com>
2023-02-12 19:22:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCountries }  from './fetchCountries'; 
-
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -12,48 +11,75 @@ const containerRef = document.querySelector('.country-info');
 
 inputRef.addEventListener('input', debounce(handleInputValue, DEBOUNCE_DELAY));
 
-
 function handleInputValue(event) {
-const inputValue = event.target.value.trim(); 
+  const inputValue = event.target.value.trim();
 
-if(inputValue.length === 1) {
-    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-} else  if(inputValue.length === 2) {
-    fetchCountries(inputValue).then(onRenderListCountries).catch(onFetchError);
-} else  if(inputValue.length >= 3) {
-    fetchCountries(inputValue).then(onRenderContainerOfCountry).catch(onFetchError);  
-} 
-else if(inputValue === '') {
-    onClear();   
-    }
+    // if (inputValue.length === 1) {
+    //   Notiflix.Notify.info(
+    //     'Too many matches found. Please enter a more specific name.'
+    //   );
+    // } else if (inputValue.length === 2) {
+    //   fetchCountries(inputValue).then(onRenderListCountries).catch(onFetchError);
+    // } else if (inputValue.length >= 3) {
+    //   fetchCountries(inputValue)
+    //     .then(onRenderContainerOfCountry)
+    //     .catch(onFetchError);
+    // } else if (inputValue === '') {
+    //   onClear();
+    // }
+fetchCountries(inputValue).then(dataCountry => { 
+    console.log(dataCountry)
+        if (dataCountry.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          }
+           else if (dataCountry.length >= 2 && dataCountry.length <= 10) {
+           onRenderListCountries(dataCountry);
+          
+          } 
+          else if (dataCountry.length === 1) {
+        onRenderContainerOfCountry(dataCountry);
+       
+          } else if (dataCountry.status === 404) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+            onClear();
+          } else if(inputValue === '') {
+            onClear(); 
+          }
+      }).catch(onFetchError);
 }
 
 function onRenderContainerOfCountry(countries) {
-    onClear(); 
-const markup = countries.map(({name, capital, flag, population, languages}) => {
-    const nameLanguages = languages.map(language => language.name);
+  onClear();
+  const markup = countries
+    .map(({ name, capital, flag, population, languages }) => {
+      const nameLanguages = languages.map(language => language.name);
 
-    return `<div><div class="container-title"><img src='${flag}' width="50" height="30"/><h1 class="title"><b>${name}</b></h1></div><p class="text"><b>Capital:</b> ${capital}</p>
-    <p class="text"><b>Population:</b> ${population}</p><p class="text"><b>Languages:</b> ${nameLanguages.join(", ")}</p></div>`
-}).join(""); 
-containerRef.insertAdjacentHTML('beforeend', markup);    
-    
+      return `<div><div class="container-title"><img src='${flag}' width="50" height="30"/><h1 class="title"><b>${name}</b></h1></div><p class="text"><b>Capital:</b> ${capital}</p>
+    <p class="text"><b>Population:</b> ${population}</p><p class="text"><b>Languages:</b> ${nameLanguages.join(
+        ', '
+      )}</p></div>`;
+    })
+    .join('');
+  containerRef.insertAdjacentHTML('beforeend', markup);
 }
 
 function onRenderListCountries(countries) {
-    onClear();
-const markup = countries.map(({flag, name}) => {
-    return `<li class="list container-text"><img src='${flag}' width="50" height="30"/><p class="text">${name}</p></li>`  
-}).join(""); 
-listRef.insertAdjacentHTML('beforeend', markup);  
-
+  onClear();
+  const markup = countries
+    .map(({ flag, name }) => {
+      return `<li class="list container-text"><img src='${flag}' width="50" height="30"/><p class="text">${name}</p></li>`;
+    })
+    .join('');
+  listRef.insertAdjacentHTML('beforeend', markup);
 }
 
 function onFetchError() {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
-  }
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
 
 function onClear() {
-listRef.innerHTML = ""; 
-containerRef.innerHTML = ""; 
+  listRef.innerHTML = '';
+  containerRef.innerHTML = '';
 }
